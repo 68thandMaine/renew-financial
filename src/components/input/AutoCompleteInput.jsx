6548e89dd12a38921f41b722510aea3d0d09fd2e
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-11-08 17:34:10 -0800

    add a google maps places api input component

---
+++ src/components/input/AutoCompleteInput.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+/* global google */
+
+import VerificationServices from '../../services/VerificationServices';
+
+class AutoCompleteInput extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.autocompleteInput = React.createRef();
+    this.address = null;
+    this.handlePlaceChanged = this.handlePlaceChanged.bind(this);
+  }
+
+  componentDidMount() {
+    this.address = new google.maps.places.Autocomplete(this.autocompleteInput.current,
+        {"types": ["address"]});
+
+    this.address.addListener('place_changed', this.handlePlaceChanged);
+  }
+
+  async handlePlaceChanged(){
+    const place = await this.address.getPlace();
+    this.props.handleSetAddressState(place.formatted_address) 
+  }
+
+  render() {
+    return (
+        <input ref={this.autocompleteInput} defaultValue={this.props.value}  className='formInput' id="address" name='address' placeholder="Enter your address"
+         type="text"></input>
+    );
+  }
+}
+
+export default AutoCompleteInput;
